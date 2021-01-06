4795860caab597c9b6d732fa7a046ae5723cc8ab
dan murphy <danielmurph8@gmail.com>
2021-01-06 16:25:10 -0500

    restructuring: add private methods for extracting data from JSON and building a dictionary representation of that extracted data

--- equit_ease/parser/parse.py
+++ equit_ease/parser/parse.py
@@ -1,6 +1,6 @@
-from typing import Dict, Any
+from __future__ import annotations
+from typing import Dict, Any, List
 import json
-import dataclasses
 
 from equit_ease.reader.read import Reader
 from equit_ease.datatypes.equity_meta import EquityMeta
@@ -13,7 +13,7 @@ class Parser(Reader):
         self.data = data
 
     @staticmethod
-    def extract_data_from(json_data: Dict[str, Any], key_to_extract: str) -> Any:
+    def _extract_data_from(json_data: Dict[str, Any], key_to_extract: str) -> Any:
         """
         extract ``key_to_extract`` from ``json_data``
 
@@ -27,10 +27,30 @@ class Parser(Reader):
             result = json_data[key_to_extract]
         return result
 
+    def _build_dict_repr(
+        self, keys_to_extract: List[str], data: Dict[str, Any]
+    ) -> Dict[str, Any]:
+        """
+        build dictionary representation with the keys to extract and the
+        overarching  JSON data structure to extract from.
+
+        :param keys_to_extract -> ``List[str]``: keys to extract.
+        :param data -> ``Dict[str, Any``: the data structure to extract from.
+
+        :returns result -> ``Dict[str, Any]``: compiled dictionary containing the keys and their extracted values.
+        """
+        finalized_data_struct = {}
+
+        for key in list(keys_to_extract):
+            finalized_data_struct[key] = self._extract_data_from(data, key)
+
+        return finalized_data_struct
+
+
 class QuoteParser(Parser):
     """contains methods relating to the parsing of Yahoo Finance Quote data."""
-    
-    def extract_equity_meta_data(self):
+
+    def extract_equity_meta_data(self: QuoteParser) -> Dict[str, Any]:
         """
         extracts meta-data from the GET /quote API call. This meta-data will
         then be used to display a tabular representation of the data in the
@@ -41,17 +61,34 @@ class QuoteParser(Parser):
         """
         equity_metadata = self.data
 
-        keys_to_extract_from_yfinance = Constants.yahoo_finance_quote_keys
+        keys_to_extract = Constants.yahoo_finance_quote_keys
         json_data_for_extraction = equity_metadata["quoteResponse"]["result"][0]
 
-        equity_meta_mappings = {}
+        equity_meta_data_struct = self._build_dict_repr(
+            keys_to_extract, json_data_for_extraction
+        )
 
-        for key in list(keys_to_extract_from_yfinance):
-            equity_meta_mappings[key] = self.extract_data_from(
-                json_data_for_extraction, key
-            )
+        return json.dumps(equity_meta_data_struct)
 
-        return json.dumps(equity_meta_mappings)
 
 class ChartParser(Parser):
     """contains methods relating to the parsing of Yahoo Finance Chart data."""
+
+    def extract_equity_chart_data(self: ChartParser) -> Dict[str, Any]:
+        """
+        extracts chart-related data from GET /chart API call. This chart data
+        is then used to build a graphical representation of the stock price and/or
+        volume (x-axis is time, y-axis is price | volume)
+        """
+        equity_chart_data = self.data
+
+        json_data_for_extraction = equity_chart_data["chart"]["result"][0][
+            "indicators"
+        ]["quote"][0]
+        keys_to_extract = json_data_for_extraction.keys()
+
+        equity_chart_data_struct = self._build_dict_repr(
+            keys_to_extract, json_data_for_extraction
+        )
+
+        return json.dumps(equity_chart_data_struct)
