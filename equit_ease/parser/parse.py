cf1e560c180f97ce7a13b115890bd881eaa5cd2f
dan murphy <danielmurph8@gmail.com>
2021-01-06 14:30:01 -0500

    restuctturing: two subclasses now inherit from the Parser class

--- equit_ease/parser/parse.py
+++ equit_ease/parser/parse.py
@@ -1,4 +1,6 @@
 from typing import Dict, Any
+import json
+import dataclasses
 
 from equit_ease.reader.read import Reader
 from equit_ease.datatypes.equity_meta import EquityMeta
@@ -6,44 +8,50 @@ from equit_ease.utils.Constants import Constants
 
 
 class Parser(Reader):
-    def __init__(self, equity_to_search, quote_data, chart_data):
+    def __init__(self, equity_to_search, data):
         super().__init__(equity_to_search)
-        self.quote_data = quote_data
-        self.chart_data = chart_data
+        self.data = data
 
+    @staticmethod
+    def extract_data_from(json_data: Dict[str, Any], key_to_extract: str) -> Any:
+        """
+        extract ``key_to_extract`` from ``json_data``
+
+        :param json_data -> ``Dict[str, Any]``: JSON response object from any GET /<yahoo_finance_endpoint> which returns JSON data.
+        :param key_to_extract -> ``str``: the key to extract from the JSON object.
+        :returns result -> ``str`` || ``int``: the value extracted from the key.
+        """
+        if key_to_extract not in json_data.keys():
+            result = "N/A"
+        else:
+            result = json_data[key_to_extract]
+        return result
+
+class QuoteParser(Parser):
+    """contains methods relating to the parsing of Yahoo Finance Quote data."""
+    
     def extract_equity_meta_data(self):
         """
         extracts meta-data from the GET /quote API call. This meta-data will
         then be used to display a tabular representation of the data in the
         console.
 
-        :params -> ``Parser``:
+        :params self -> ``Parser``:
         :returns -> ``EquityMeta``: dataclass defined in datatypes/equity_meta.py
         """
-        equity_metadata = self.quote_data
+        equity_metadata = self.data
 
-        keys_to_extract = Constants.yahoo_finance_quote_keys
+        keys_to_extract_from_yfinance = Constants.yahoo_finance_quote_keys
+        json_data_for_extraction = equity_metadata["quoteResponse"]["result"][0]
+
         equity_meta_mappings = {}
 
-        def extract_data_from(
-            equity_metadata: Dict[str, Any], key_to_extract: str
-        ) -> str or int:
-            """
-            extract ``key_to_extract`` from ``equity_metadata``
-
-            :param equity_metadata -> ``Dict[str, Any]``: JSON response object from GET /quote (see Reader.get_equity_quote_data)
-            :param key_to_extract -> ``str``: the key to extract from the JSON object.
-            :returns result -> ``str`` || ``int``: the value extracted from the key.
-            """
-            if key_to_extract not in equity_metadata.keys():
-                result = "N/A"
-            else:
-                result = equity_metadata[key_to_extract]
-            return result
-
-        for key in list(keys_to_extract):
-            equity_meta_mappings[key] = extract_data_from(
-                equity_metadata["quoteResponse"]["result"][0], key
+        for key in list(keys_to_extract_from_yfinance):
+            equity_meta_mappings[key] = self.extract_data_from(
+                json_data_for_extraction, key
             )
 
-        return equity_meta_mappings
+        return json.dumps(equity_meta_mappings)
+
+class ChartParser(Parser):
+    """contains methods relating to the parsing of Yahoo Finance Chart data."""
