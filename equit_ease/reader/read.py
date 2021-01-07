49a53dfab318b3a73a09e5ba6f4c3d2ebcc78d83
dan murphy <danielmurph8@gmail.com>
2021-01-07 16:58:23 -0500

    re-structuring read.py to support different __init__ process

--- equit_ease/reader/read.py
+++ equit_ease/reader/read.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from os import name
 from typing import Dict, Any
 import requests
 
@@ -18,13 +19,11 @@ class Reader:
     """
 
     def __init__(
-        self, equity_to_search: str, date_range: str = None, currency: str = None
+        self, equity: str
     ) -> None:
         # Method Resolution Order: https://stackoverflow.com/questions/42413670/whats-the-difference-between-super-and-parent-class-name
         super().__init__()
-        self.equity_to_search = equity_to_search
-        self.date_range = date_range
-        self.currency = currency
+        self.equity = equity
 
     def _get(self, y_finance_formatted_url: str) -> Dict[str, Any]:
         """
@@ -41,8 +40,28 @@ class Reader:
         response.raise_for_status()
 
         return response.json()
+    
+    @property
+    def ticker(self: Reader) -> str:
+        """getter for the ticker attribute."""
+        return self.__ticker
+    
+    @ticker.setter
+    def ticker(self: Reader, ticker_value: str) -> None:
+        """setter for the ticker attribute."""
+        self.__ticker = ticker_value
 
     @property
+    def name(self: Reader) -> str:
+        """getter for the name attribute."""
+        return self.__name
+    
+    @name.setter
+    def name(self: Reader, name_value: str) -> None:
+        """setter for the name attribute."""
+        self.__name = name_value
+
+
     def build_equity_chart_url(self: Reader) -> str:
         """
         Creates the equity chart URL for a given currency.
@@ -54,7 +73,7 @@ class Reader:
         """
         base_chart_url = Constants.yahoo_finance_base_chart_url
         # TODO: this will be more robust based off args that can be passed via command-line
-        result = base_chart_url + self.equity_to_search
+        result = base_chart_url + self.equity
 
         def is_valid(ticker_url: str) -> str:
             """
@@ -71,10 +90,10 @@ class Reader:
             return True
         raise ValueError("Invalid Ticker Symbol Passed.")
 
-    @property
+
     def build_equity_quote_url(self: Reader) -> str:
         """
-        Creates the equity quote URL for a given currency.
+        Creates the quote URL for a given equity.
         This URL is then used for the retrieval of data-points pertaining to
         equity meta-data such as EPS, P/E ratio, 52 wk high and low, etc...
 
@@ -83,7 +102,7 @@ class Reader:
         """
         base_quote_url = Constants.yahoo_finance_base_quote_url
         # TODO: this will be more robust based off args that can be passed via command-line
-        result = base_quote_url + f"?symbols={self.equity_to_search}"
+        result = base_quote_url + f"?symbols={self.equity}"
 
         def is_valid(ticker_url: str) -> str:
             """
@@ -102,6 +121,9 @@ class Reader:
             return True
         raise ValueError("Invalid Ticker Symbol Passed.")
 
+    def build_company_lookup_url(self):
+        """"""
+
     def get_equity_chart_data(self: Reader) -> str:
         """
         calls the _get() private method.
@@ -117,3 +139,22 @@ class Reader:
         :returns -> ``Dict[str, Any]``: JSON object response from Yahoo Finance
         """
         return self._get(self.quote_url)
+    
+    def get_equity_company_data(self: Reader) -> str:
+        """
+        the 'equity' value passed upon initialization is used to perform a 
+        'reverse lookup'. 
+        
+        The 'equity' value is used to query a Yahoo Finance endpoint which 
+        returns the shortname and ticker symbol (amongst other things) for
+        a stock. These two attributes are set with getter/setter methods
+        and the ticker symbol is then used throughout the hierarchical
+        structure to query yahoo finance.
+
+        :param self -> ``Reader``:
+        :returns result -> ``Dict[str, Any]``: Dict containing shortname and ticker symbol data.
+
+        #TODO: when CLI is setup, lets take the list of companies returned from the reverse lookup and allow the user to select which one they want to see. 
+        # ! kinda like how GitHub offers various authentication options and you get to choose.
+        """
+
