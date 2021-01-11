325e991b6bc320b06fe403593f65ea58e2fcab87
dan murphy <danielmurph8@gmail.com>
2021-01-11 13:03:46 -0500

    add unit tests for extract_equity_meta_data

--- tests/test_quote_parser.py
+++ tests/test_quote_parser.py
@@ -1,11 +1,25 @@
+import dataclasses
+from equit_ease.datatypes.equity_meta import EquityMeta
+from equit_ease.utils.Constants import Constants
 import unittest
+import json
+import os
 
 from equit_ease.parser.parse import QuoteParser
+
+def read_quote_fixture():
+    fixture_file_path = os.path.join(
+        os.path.dirname(__file__),
+        "fixtures/quote.json"
+    )
+    with open(fixture_file_path, "r") as quote_fixture:
+        data = json.loads(quote_fixture.read())
+    return data
 
 class TestQuoteParserMethods(unittest.TestCase):
     def setUp(self):
         self.equity = "Apple"
-        self.data_fixture = {"one": "first key", "two": None}
+        self.data_fixture = read_quote_fixture()
         self.parser = QuoteParser(self.equity, self.data_fixture)
 
     def tearDown(self):
@@ -13,8 +27,32 @@ class TestQuoteParserMethods(unittest.TestCase):
         self.data_fixture = None
         self.parser = QuoteParser
     
-    def test_extract_equity_meta_data(self):
+    def test_extract_equity_meta_data_instance_type(self):
         """
         test extract_equity_meta_data() internal method #1 -> pass.
+        
+        check that the response from extract_equity_meta_data() is a
+        EquityMeta dataclass instance with the expected field names.
         """
-        return True
+        equity_meta = self.parser.extract_equity_meta_data()
+        received_field_names = [field.name for field in dataclasses.fields(equity_meta)]
+        expected_field_names = [field.name for field in dataclasses.fields(EquityMeta)]
+
+        self.assertIsInstance(equity_meta, EquityMeta)
+        self.assertEqual(received_field_names, expected_field_names)
+    
+    def test_extract_equity_metadata_validity(self):
+        """
+        test extract_equity_meta_data() method #2 -> pass.
+
+        this time, checking for the validity of the data that is
+        mapped from JSON -> the dataclass.
+        """
+        equity_meta_as_dict = dataclasses.asdict(self.parser.extract_equity_meta_data())
+        quote_fixture = read_quote_fixture()
+        data_for_validation = quote_fixture['quoteResponse']['result'][0]
+        column_mappings = Constants.yahoo_finance_column_mappings
+
+        for dataclass_key, json_data_key in column_mappings.items():
+            self.assertEqual(equity_meta_as_dict[dataclass_key], data_for_validation[json_data_key])
+        
