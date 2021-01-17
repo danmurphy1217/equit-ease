5269965d657e0fdf7d5e60ffc8d2d6e7c12b9a60
dan murphy <danielmurph8@gmail.com>
2021-01-17 16:50:35 -0500

    add methods to Reader class

--- equit_ease/reader/read.py
+++ equit_ease/reader/read.py
@@ -55,6 +55,44 @@ class Reader:
         else:
             result = json_data[key_to_extract]
         return result
+    
+    def set_ticker_and_name_props_to(self, ticker_val: str, name_val: str) -> None:
+        """
+        handle setting the `ticker` and `name` props for the class
+        instance. Since these two setters are called in tandom, it 
+        makes most sense to extrapolate them to a public method
+        that is called with the values to set.
+
+        :param reader -> ``Reader``: an instianted Reader class obj.
+        :param ticker_val -> ``str``: the value to set to the ticker property.
+        :param name_val -> ``str``: the value to set to the name property.
+        :returns -> ``None``: Modifies the current reader object, therefore
+                                no need to return it.
+        """
+        self.ticker = ticker_val
+        self.name = name_val
+    
+    def build_urls(self):
+        """
+        calls private methods that build the Quote and Chart URLs.
+
+        :parm self -> ``Reader``:
+        :return -> ``None``:
+        """
+        self.build_equity_quote_url()
+        self.build_equity_chart_url()
+    
+    def get_data(self):
+        """
+        public interface used to call the private methods that retrieve
+        quote and chart-related datapoints.
+
+        :param self -> ``Reader``:
+        """
+        equity_quote_data = self.get_equity_quote_data()
+        equity_chart_data = self.get_equity_chart_data()
+
+        return equity_quote_data, equity_chart_data
 
     @property
     def ticker(self: Reader) -> str:
