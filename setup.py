7ab05ec209f884bcae94b0abb8d7a73370209b91
dan murphy <danielmurph8@gmail.com>
2021-01-18 21:21:23 -0500

    update setup.py with accurate scripts, long description, inline version (for now), entry_point, and requirements

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ import setuptools
 #     re.MULTILINE
 # ).group(1)
 
-print(setuptools.find_packages())
 
 long_description = open("README.md").read()
 
+
 setuptools.setup(
     name="EquitEase",
-    version= '0.0.1',
+    version= '0.0.3',
     url="https://github.com/danmurphy1217/equit-ease",
     entry_points={
         "console_scripts": ['equity=equit_ease.main:run']
@@ -31,6 +31,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages= setuptools.find_packages(),
-    install_requires = [*open("requirements.txt").read().splitlines()],
+    install_requires = ['appdirs==1.4.4', 'attrs==20.3.0', 'autopep8==1.5.4', 'black==20.8b1', 'certifi==2020.12.5', 'chardet==4.0.0', 'click==7.1.2', 'docutils==0.16', 'idna==2.10', 'iniconfig==1.1.1', 'mypy-extensions==0.4.3', 'packaging==20.8', 'pathspec==0.8.1', 'pluggy==0.13.1', 'prompt-toolkit==1.0.14', 'py==1.10.0', 'pycodestyle==2.6.0', 'Pygments==2.7.4', 'PyInquirer==1.0.3', 'pyparsing==2.4.7', 'regex==2020.11.13', 'requests==2.25.1', 'rstcheck==3.3.1', 'six==1.15.0', 'toml==0.10.2', 'typed-ast==1.4.2', 'typing-extensions==3.7.4.3', 'urllib3==1.26.2', 'wcwidth==0.2.5'],
     python_requires = ">= 3.7"
 )
+
