4dd213d2fa0387f4b86079f4080c72834cceef74
dan murphy <danielmurph8@gmail.com>
2021-01-25 17:44:05 -0500

    setup unit tests for UserConfigParser

--- tests/TODO/parser/test_user_config_parser.py
+++ tests/TODO/parser/test_user_config_parser.py
@@ -1,6 +1,27 @@
 import unittest
+import json
+import os
+from pathlib import Path
 
 from equit_ease.parser.parse import UserConfigParser
 
 class TestUserConfigParser(unittest.TestCase):
     """Testing methods from the UserConfigParser class."""
+    user_home_dir = str(Path.home()) 
+    equit_ease_dir = os.path.join(user_home_dir, ".equit_ease")
+    lists_file_path = Path(os.path.join(equit_ease_dir, "lists"))
+
+    def setUp(self):
+        self.list_name = "Test"
+        self.list_file_contents = open(self.lists_file_path, "r").read().splitlines()
+        self.parser = UserConfigParser(self.list_name, self.list_file_contents)
+        self.equities, _ = self.parser.format_equity_lists()
+
+    def tearDown(self):
+        self.equity = None
+        self.list_file_contents = None
+        self.parser = UserConfigParser
+        self.equities = None
+    
+
+
